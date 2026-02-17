c2fe0019d2ec3b1bcd5503bd32260d6cfc54322d
Haim Aflalo <kodkodhaim@gmail.com>
2026-02-17 17:02:40 +0200

    tet

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import { complaintsRouter } from './routes/complaints.router.js';
 import { adminRouter } from './routes/admin.router.js';
 import { connectMongo } from './db/connect.js';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/api', complaintsRouter);
 app.use('/api', adminRouter);
